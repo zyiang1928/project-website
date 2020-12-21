01a3943a81c77acf123834672956b4ed738352f8
zyiang1928 <dryant732@gmail.com>
2020-12-21 18:28:52 +0700

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 app.get("/", function(request, response) {
   console.log("[Website] Pinging!");
   response.sendStatus(200);
-  response.sendFile(__dirname + "./index.html");
+  response.sendFile(__dirname + "/index.html");
 });
 server.listen(process.env.PORT, function() {
   console.log(`[Website] Listening on port: ${process.env.PORT}`);
