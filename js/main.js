82a127c4a0d0760c546302e13e658b5ad133aa55
zyiang1928 <dryant732@gmail.com>
2021-01-30 14:23:36 +0700

    update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,5 @@ console.log("[Website] Launching...");
 $(document).ready(function() {
   setTimeout(function() {
     $('body').addClass('loaded');
-    $('h1').css('color', '#222222')
-  }, 1000);
+  }, 5000);
 });
